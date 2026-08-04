e869d11ddd2bed064158536c25d9acde70d30fb0
dazulrich <dazulrich@gmail.com>
2026-08-04 16:39:50 +0100

    Update ofgwrite.c

--- ofgwrite.c
+++ ofgwrite.c
@@ -1389,28 +1389,17 @@ int umount_rootfs(int steps)
 	sleep(3);
 
 	ret = umount("/oldroot/newroot");
+	ret = umount("/oldroot/");
 	if (!ret)
 	{
 		my_printf("umount successful\n");
-		set_error_text1("newroot umount successful");
+		set_error_text1("umount successful");
 		sleep(1);
 	}	
 	else
 	{
 		my_printf("umount not successful\n");
-		set_error_text1("newroot umount not successful");
-		sleep(1);
-	}
-	ret = umount("/oldroot/");
-	if (!ret)
-	{
-		my_printf("umount successful\n");
-		set_error_text1("oldroot umount successful");
-		sleep(1);
-	}
-	else
-		my_printf("umount not successful\n");
-		set_error_text1("oldroot umount not successful");
+		set_error_text1("umount not successful");
 		sleep(1);
 	}
 
