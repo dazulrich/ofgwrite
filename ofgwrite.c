185f1166fbb6283d58d38f3956eeb7371375b261
dazulrich <dazulrich@gmail.com>
2026-08-20 21:12:00 +0100

    Update ofgwrite.c

--- ofgwrite.c
+++ ofgwrite.c
@@ -1366,7 +1366,7 @@ int umount_rootfs(int steps)
 		umount2(oldroot_path, MNT_DETACH);
 		free(mountlist->dir);
 		mountlist = mountlist->next;
-		sleep(1);
+		//sleep(1);
 	}
 
 	// create link for mount/umount for autofs
@@ -1391,13 +1391,13 @@ int umount_rootfs(int steps)
 	sleep(3);
 
 	ret = umount("/oldroot/newroot");
-	sleep(1);
+	//sleep(1);
 	ret = umount("/oldroot/");
 	if (!ret)
 		my_printf("umount successful\n");
 	else
 		my_printf("umount not successful\n");
-	sleep(1);
+	//sleep(1);
 	// mount oldroot to other mountpoint, because otherwise all data in not moved filesystems under /oldroot will be deleted
 	if (rootfs_flash_mode == TARBZ2 || rootfs_flash_mode == TARBZ2_MTD || rootfs_flash_mode == UBI_LOOP_SUBDIR || rootfs_flash_mode == TARXZ_UBI)
 	{
